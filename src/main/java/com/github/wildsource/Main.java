8f5b2ec3765aea3b93e9b09ce1ad0cf310979604
WildSource <ilovetrap123@hotmail.com>
2025-01-28 06:31:14 -0500

    setup basic voice recognition

--- src/main/java/com/github/wildsource/Main.java
+++ src/main/java/com/github/wildsource/Main.java
@@ -10,31 +10,28 @@ import javax.sound.sampled.Mixer;
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        for (Mixer.Info info : AudioSystem.getMixerInfo()) {
-            System.out.println(info.getName() + " - " + info.getDescription());
-        }
+	public static void main(String[] args) throws IOException {
+		for (Mixer.Info info : AudioSystem.getMixerInfo()) {
+			System.out.println(info.getName() + " - " + info.getDescription());
+		}
 
-        /*Configuration configuration = new Configuration();
+		Configuration configuration = new Configuration();
 
-        configuration.setAcousticModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us");
-        configuration.setDictionaryPath("resource:/edu/cmu/sphinx/models/en-us/cmudict-en-us.dict");
-        configuration.setLanguageModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us.lm.bin");
+		configuration.setAcousticModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us");
+		configuration.setDictionaryPath("resource:/edu/cmu/sphinx/models/en-us/cmudict-en-us.dict");
+		configuration.setLanguageModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us.lm.bin");
 
-        LiveSpeechRecognizer recognizer = new LiveSpeechRecognizer(configuration);
-        // Start recognition process pruning previously cached data.
-        recognizer.startRecognition(true);
-        SpeechResult result = recognizer.getResult();
-        // Pause recognition process. It can be resumed then with startRecognition(false).
-        recognizer.stopRecognition();
+		LiveSpeechRecognizer recognizer = new LiveSpeechRecognizer(configuration);
+		// Start recognition process pruning previously cached data.
+		recognizer.startRecognition(true);
+		
+		while (true) {
+			SpeechResult result = recognizer.getResult();
+			System.out.println(result.getHypothesis());
+		}
 
-        // Print utterance string without filler words.
-        System.out.println(result.getHypothesis());
-
-
-        // Get individual words and their times.
-        for (WordResult r : result.getWords()) {
-            System.out.println(r);
-        }*/
-    }
+		// Pause recognition process. It can be resumed then with
+		// startRecognition(false).
+		// recognizer.stopRecognition();
+	}
 }
